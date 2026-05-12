cec59b067e96ccc4a6a3a423e214c392b938223b
angiemarp-byte <angiemarpf@gmail.com>
2026-05-11 19:59:44 -0500

    11

---
+++ Assets/Script/TurnSwitchController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TurnSwitchController : MonoBehaviour
+{
+    public ActionBasedSnapTurnProvider snapTurnProvider;
+    public ActionBasedContinuousTurnProvider continuousTurnProvider;
+
+    public bool snapTurnActive = true;
+
+    public void SwitchTurn()
+    {
+        if (snapTurnActive == true)
+        {
+            snapTurnProvider.turnAmount = 0;
+            continuousTurnProvider.turnSpeed = 60;
+
+            snapTurnActive = false;
+        }
+        else
+        {
+            snapTurnProvider.turnAmount = 15;
+            continuousTurnProvider.turnSpeed = 0;
+
+            snapTurnActive = true;
+        }
+    }
+}
